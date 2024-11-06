3297283f6311cbd2c430e6296545baaf8336a32c
Stanislav Zhmurin <zhmur96@gmail.com>
2024-11-06 19:46:53 +0300

    commit

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+    // Показывать ошибки
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    function debug($data, $isDie = true, $html = false) {
-        if ($html) echo '<pre>' . var_export($data, return: true) . '</pre>';
-        else echo "\n" . var_export($data, true) . "\n";
+    // дебагер
+    function debug($data, $isDie = true) {
+        echo "\n" . var_export($data, true) . "\n";
         if ($isDie) {
             die();
         }
     }
 
     include("autoload.php");
-    
+
     $migration = new migration();
